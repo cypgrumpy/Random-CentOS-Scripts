619d69e7063df8657b3602290979e6295bb3c190
Andrew <grumpy@cypherservers.com>
2014-07-27 21:04:18 -0500

    Made the script less shit.
    I forgot that you can't actually read what's going by without a pause.
    Removed the useless "contains" function, edited some lines to make them
    more friendly. Made sure that the ID key and password were outputted.

--- kvm.sh
+++ kvm.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 clear
-contains ()
-{}
 echo "INSTALLIN YO SHIT LIKE YOU WOULD.  THIS IS PROBABLY KVM AND SVM SLAVE!"
-
-yum install kvm qemu-kvm python-virtinst libvirt libvirt-python virt-manager libguestfs-tools
+read -sn 1 -p "Press any key to continue..."
+yum install kvm qemu-kvm python-virtinst libvirt libvirt-python virt-manager libguestfs-tools -y
 chkconfig libvirtd on
 service libvirtd start
 clear
-echo "Okay, you have to configure the network yourself.  This is a fairly easy task.  If you're an idiot and don't know how, this is a good guide: http://docs.solusvm.com/kvm_bridge_setup once you've done that, come back and restart this script."
-
-files.us.tx.soluslabs.com
+echo "Okay, you have to configure the network yourself.  This is a fairly easy task.  If you don't know how, this is a good guide: http://docs.solusvm.com/kvm_bridge_setup once you've done that, come back and restart this script."
+echo "Escape key is ^C"
+read -sn 1 -p "Press any key to continue..."
 echo "Please Wait, Installing Dependencies for RHEL 6..."
 yum -y install glibc glibc-devel glibc-devel.i686 perl php php-cli php-common
 wget files.us.tx.soluslabs.com/solusvm/loaders/ioncube_loaders_lin_x86_64.tar.gz -O /usr/local/solusvm/tmp/ioncube_loaders.tar.gz
@@ -26,5 +24,5 @@ tar vxzf /usr/local/solusvm/tmp/installscripts.tar.gz -C /usr/local/solusvm/tmp/
 chown root:root /usr/local/solusvm/tmp/.install/*
 chmod +x /usr/local/solusvm/tmp/.install/*
 php /usr/local/solusvm/tmp/.install/slave --virt=kvm --templates=none
-clear
-ech "Thankyou for using the world's messiest script."
+
+echo "Thankyou for using the world's messiest script."
